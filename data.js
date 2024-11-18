6d6d9c68a8f4a0169035e7b678414e22bb2aaf3a
VidanovaVeronika <vidanovanika@gmail.com>
2024-11-18 20:15:56 +0300

    laba 4,5

--- data.js
+++ data.js
@@ -6,6 +6,7 @@ const menu = [
     keyword: "gaspacho",
     weight: "350г",
     category: "soup",
+    kind: "veg",
   },
   {
     img: "./src/soups/mushroom_soup.jpg",
@@ -14,6 +15,7 @@ const menu = [
     keyword: "mushroom",
     weight: "330г",
     category: "soup", 
+    kind: "veg",
   },
   {
     img: "./src/soups/norwegian_soup.jpg",
@@ -22,6 +24,34 @@ const menu = [
     keyword: "norvezskiy",
     weight: "330г",
     category: "soup", 
+    kind: "fish",
+  },
+  {
+    img: "./src/soups/chicken.jpg",
+    price: "330 &#8381",
+    name: "Куриный суп",
+    keyword: "chicken_soup",
+    weight: "350г",
+    category: "soup", 
+    kind: "meat",
+  },
+  {
+    img: "./src/soups/ramen.jpg",
+    price: "375 &#8381",
+    name: "Рамен",
+    keyword: "ramen",
+    weight: "425г",
+    category: "soup", 
+    kind: "meat",
+  },
+  {
+    img: "./src/soups/tomyum.jpg",
+    price: "650 &#8381",
+    name: "Том ям с креветками",
+    keyword: "tomyum",
+    weight: "500г",
+    category: "soup", 
+    kind: "fish"
   },
   {
     img: "./src/main_course/friedpotatoeswithmushrooms1.jpg",
@@ -30,6 +60,7 @@ const menu = [
     keyword: "friedpotato",
     weight: "210г",
     category: "main_dish", 
+    kind: "veg"
   },
   {
     img: "./src/main_course/lasagna.jpg",
@@ -38,6 +69,7 @@ const menu = [
     keyword: "lasagna",
     weight: "310г",
     category: "main_dish", 
+    kind: "meat"
   },
   {
     img: "./src/main_course/chickencutletsandmashedpotatoes.jpg",
@@ -45,7 +77,89 @@ const menu = [
     name: "Котлеты из курицы с картофельным пюре",
     keyword: "chicken",
     weight: "280г",
+    category: "main_dish",
+    kind: "meat" 
+  },
+  {
+    img: "./src/main_course/fishrice.jpg",
+    price: "320 &#8381",
+    name: "Рыбная котлета с рисом и спаржей",
+    keyword: "fishrice",
+    weight: "270г",
     category: "main_dish", 
+    kind: "fish"
+  },
+  {
+    img: "./src/main_course/pizza.jpg",
+    price: "450 &#8381",
+    name: "Пицца Маргарита",
+    keyword: "pizza",
+    weight: "470г",
+    category: "main_dish",
+    kind: "veg" 
+  },
+  {
+    img: "./src/main_course/shrimppasta.jpg",
+    price: "340 &#8381",
+    name: "Паста с креветками",
+    keyword: "shrimppasta",
+    weight: "280г",
+    category: "main_dish", 
+    kind: "fish"
+  },
+  {
+    img: "./src/salads_starters/caesar.jpg",
+    price: "370 &#8381",
+    name: "Цезарь с цыпленком",
+    keyword: "caesar",
+    weight: "220г",
+    category: "salad_starter",
+    kind: "meat"
+  },
+  {
+    img: "./src/salads_starters/caprese.jpg",
+    price: "350 &#8381",
+    name: "Капрезе с моцареллой",
+    keyword: "caprese",
+    weight: "235г",
+    category: "salad_starter", 
+    kind: "veg"
+  },
+  {
+    img: "./src/salads_starters/frenchfries1.jpg",
+    price: "280 &#8381",
+    name: "Картофель фри с соусом Цезарь",
+    keyword: "frenchfries_caesar",
+    weight: "235г",
+    category: "salad_starter", 
+    kind: "veg"
+  },
+  {
+    img: "./src/salads_starters/frenchfries2.jpg",
+    price: "260 &#8381",
+    name: "Картофель фри с кетчупом",
+    keyword: "frenchfries",
+    weight: "235г",
+    category: "salad_starter", 
+    kind: "veg"
+  },
+  {
+    img: "./src/salads_starters/saladwithegg.jpg",
+    price: "330 &#8381",
+    name: "Корейский салат с овощами и яйцом",
+    keyword: "saladwithegg",
+    weight: "250г",
+    category: "salad_starter",
+    kind: "veg" 
+  },
+  {
+    img: "./src/salads_starters/tunasalad.jpg",
+    price: "480 &#8381",
+    name: "Салат с тунцом",
+    keyword: "tunasalad",
+    weight: "250г",
+    category: "salad_starter", 
+    kind: "fish"
   },
   {
     img: "./src/beverages/orangejuice.jpg",
@@ -53,7 +167,8 @@ const menu = [
     name: "Апельсиновый сок",
     keyword: "orange_juice",
     weight: "300мл",
-    category: "juice", 
+    category: "drink", 
+    kind: "cold"
   },
   {
     img: "./src/beverages/applejuice.jpg",
@@ -61,7 +176,8 @@ const menu = [
     name: "Яблочный сок",
     keyword: "apple_juice",
     weight: "300мл",
-    category: "juice", 
+    category: "drink", 
+    kind: "cold"
   },
   {
     img: "./src/beverages/carrotjuice.jpg",
@@ -69,7 +185,89 @@ const menu = [
     name: "Морковный сок",
     keyword: "carrot_juice",
     weight: "300мл",
-    category: "juice", 
+    category: "drink", 
+    kind: "cold"
+  },
+  {
+    img: "./src/beverages/cappuccino.jpg",
+    price: "180 &#8381",
+    name: "Капучино",
+    keyword: "capuccino",
+    weight: "300мл",
+    category: "drink", 
+    kind: "hot"
+  },
+  {
+    img: "./src/beverages/greentea.jpg",
+    price: "100 &#8381",
+    name: "Зеленый чай",
+    keyword: "greentea",
+    weight: "300мл",
+    category: "drink", 
+    kind: "hot"
+  },
+  {
+    img: "./src/beverages/tea.jpg",
+    price: "90 &#8381",
+    name: "Черный чай",
+    keyword: "tea",
+    weight: "300мл",
+    category: "drink", 
+    kind: "hot"
+  },
+  {
+    img: "./src/desserts/baklava.jpg",
+    price: "220 &#8381",
+    name: "Пахлава",
+    keyword: "paklava",
+    weight: "300гр",
+    category: "dessert", 
+    kind: "average"
+  },
+  {
+    img: "./src/desserts/checheesecake.jpg",
+    price: "240 &#8381",
+    name: "Чизкейк",
+    keyword: "checheesecake",
+    weight: "125гр",
+    category: "dessert", 
+    kind: "small"
+  },
+  {
+    img: "./src/desserts/chocolatecake.jpg",
+    price: "270 &#8381",
+    name: "Шоколадный торт",
+    keyword: "chocolatecake",
+    weight: "140гр",
+    category: "dessert", 
+    kind: "small"
+  },
+  {
+    img: "./src/desserts/chocolatecheesecake.jpg",
+    price: "260 &#8381",
+    name: "Шоколадный чизкейк",
+    keyword: "chocolatecheesecake",
+    weight: "125гр",
+    category: "dessert",
+    kind: "small" 
+  },
+  {
+    img: "./src/desserts/donuts.jpg",
+    price: "650 &#8381",
+    name: "Пончики (6 штук)",
+    keyword: "donuts6",
+    weight: "700гр",
+    category: "dessert", 
+    kind: "big"
+  },
+  {
+    img: "./src/desserts/donuts2.jpg",
+    price: "410 &#8381",
+    name: "Пончики (3 штуки)",
+    keyword: "donuts3",
+    weight: "700гр",
+    category: "dessert", 
+    kind: "average"
   }
 ];
 
--- make_lunch.js
+++ make_lunch.js
@@ -1,49 +1,106 @@
 import menu from "./data.js";
 
+menu.sort((a, b) => a.name > b.name ? 1 : -1);
 
-for (let i = 0; i < menu.length; i++) {
-  //создаем блок dish_elem
-  let dishElem = document.createElement("div");
-  dishElem.classList.add('dish_elem');
-  dishElem.dataset.dish = menu[i].category;
-  // создаем картинку
-  let img = document.createElement('img');
-  img.src = menu[i].img;
-  img.alt = menu[i].category;
-  dishElem.appendChild(img);
+const orders = document.getElementById('orders');
+orders.hidden = true;
+const order_price = document.getElementById('order_price');
+const no_order = document.getElementById('no_dishes');
 
-  // создаем описание
-  let price = document.createElement('p');
-  price.classList.add('price');
-  price.innerHTML = menu[i].price;
-  dishElem.appendChild(price);
-
-  let name = document.createElement('p');
-  name.classList.add('name');
-  name.innerHTML = menu[i].name;
-  dishElem.appendChild(name);
-
-  let weight = document.createElement('p');
-  weight.classList.add('weight');
-  weight.innerHTML = menu[i].weight;
-  dishElem.appendChild(weight);
-
-  // создаем кнопку
-  let button = document.createElement('button');
-  button.classList.add('add_dish');
-  button.innerHTML = "Добавить";
-  dishElem.appendChild(button);
-
-  // добавляем dish_elem в dish
-  let parent = document.getElementById(`dish_${menu[i].category}`);
-  parent.appendChild(dishElem);
+let price = {
+  soup: 0,
+  main_dish: 0,
+  salad_starter: 0,
+  drink: 0,
+  dessert: 0,
+  summ: function () {
+    return this.soup + this.main_dish + this.salad_starter + this.drink +this.dessert
+  }
 }
 
 
-// вешаем слушатели событий
+const addInOrder = () => {
+  for(const elem of document.getElementsByClassName('dish_elem')) {
+    elem.addEventListener('click', () => {
+      no_order.hidden = true;
+      orders.hidden = false;
+  
+      const arr = Array.from(elem.childNodes);
+      const type_of_food = elem.dataset.dish;
+  
+      price[type_of_food] = Number(Array.from(arr[1].textContent).slice(0, -2).join(''));
+      document.getElementById(`no_${type_of_food}`).innerHTML = arr[2].textContent + " " + price[type_of_food] + "&#8381";
+      order_price.innerHTML = price.summ() + "&#8381";
+      
+      document.getElementById(`order_${type_of_food}_value`).value = arr[2].textContent;
+      document.getElementById('order_price_value').value = price.summ();
+  
+    })
+  };
+}
 
-for(const elem of document.getElementsByClassName('add_dish')) {
+function rend (paramMenu=menu, paramCategory='') {
+  for (let i = 0; i < paramMenu.length; i++) {
+    let dishElem = document.createElement("div");
+    dishElem.classList.add('dish_elem');
+    dishElem.dataset.dish = paramMenu[i].category;
+
+    let img = document.createElement('img');
+    img.src = paramMenu[i].img;
+    img.alt = paramMenu[i].category;
+    dishElem.appendChild(img);
+
+    let price = document.createElement('p');
+    price.classList.add('price');
+    price.innerHTML = paramMenu[i].price;
+    dishElem.appendChild(price);
+
+    let name = document.createElement('p');
+    name.classList.add('name');
+    name.innerHTML = paramMenu[i].name;
+    dishElem.appendChild(name);
+
+    let weight = document.createElement('p');
+    weight.classList.add('weight');
+    weight.innerHTML = paramMenu[i].weight;
+    dishElem.appendChild(weight);
+
+    let button = document.createElement('button');
+    button.classList.add('add_dish');
+    button.innerHTML = "Добавить";
+    dishElem.appendChild(button);
+
+    let parent = document.getElementById(`dish_${paramCategory === '' ? paramMenu[i].category : paramCategory}`);
+    parent.appendChild(dishElem);
+  }
+  addInOrder();
+}
+
+rend();
+
+for(const elem of document.getElementsByClassName('filter_button')) {
   elem.addEventListener('click', () => {
-    // некоторый функционал
+    const isActive = elem.dataset.active;
+    const kind = elem.dataset.kind;
+    const category = elem.parentElement.parentElement.getElementsByClassName('dish')[0].id.split('').slice(5).join('');
+    const parent = document.getElementById(`dish_${category}`);
+    Array.from(parent.getElementsByClassName('dish_elem')).forEach((el) => parent.removeChild(el));
+    if (isActive === "false") {
+      const filteredElems = menu.filter((el) => el.category === category && el.kind === kind);      
+      rend(filteredElems, category);
+      Array.from(parent.parentElement.getElementsByClassName('active_button')).forEach((el) => {
+        el.classList.remove('active_button');
+        el.dataset.active = "false";
+      });
+      elem.dataset.active = 'true';
+      elem.classList.add('active_button');
+    } else {
+      const filteredElems = menu.filter((el) => el.category === category);
+      rend(filteredElems, category);
+      elem.dataset.active = 'false';
+      elem.classList.remove('active_button');
+    }
   })
-};
+}
+
+
